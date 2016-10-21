4e66a47b9fff4fafb92c0acdc0be96e179e4dae6
Taras Kuzyo <kuzyo.taras@gmail.com>
2016-10-22 01:04:52 +0300

    logo added

--- public/app.js
+++ public/app.js
@@ -1,12 +1,14 @@
 $(document).ready(function() {
      var lock = new Auth0Lock(AUTH0_CLIENT_ID, AUTH0_DOMAIN, {
+        theme: {
+            logo: '../public/jessikaio_logo_gradient_small.png',
+            primaryColor: '#6daedb',
+        },
+        container: 'lock-box',
         auth: {
           redirectUrl: AUTH0_CALLBACK_URL
         }
      });
-
-    $('.btn-login').click(function(e) {
-      e.preventDefault();
-      lock.show();
-    });
+     lock.show();
+   
 });
