6b62a00d5da77e59f479c41af33de20673acec73
Taras Kuzyo <kuzyo.taras@gmail.com>
2016-10-22 01:26:17 +0300

    logo sizes

--- public/app.js
+++ public/app.js
@@ -1,9 +1,8 @@
 $(document).ready(function() {
      var lock = new Auth0Lock(AUTH0_CLIENT_ID, AUTH0_DOMAIN, {
         theme: {
-            logo: '../public/jessikaio_logo_gradient_small.png',
+            logo: '../public/jessikaio_logo_gradient_sm.png',
             primaryColor: '#6daedb',
-            title: ""
         },
         container: 'lock-box',
         auth: {
