99e3fe524fbfb896f73bbb07c0beadcec3a522ab
Taras Kuzyo <kuzyo.taras@gmail.com>
2016-10-26 00:46:47 +0300

    user id validation

--- server.py
+++ server.py
@@ -4,11 +4,13 @@ import json
 import requests
 import urlparse
 import psycopg2
+import psycopg2.extras
 from functools import wraps
 from flask import Flask, request, jsonify, session, redirect, render_template, send_from_directory
 from dotenv import Dotenv
 
 
+# # # # # # # # # # # # # # # # # # # # # # # # # # #
 # Load Env variables
 env = None
 
@@ -18,10 +20,15 @@ except IOError:
   env = os.environ
 
 
+# # # # # # # # # # # # # # # # # # # # # # # # # # #
+
 app = Flask(__name__, static_url_path= '')
-app.secret_key = '@mgonto'
+app.secret_key = 'wIJrsS92Hz16BwyRJiYG7p9N1ekdDj22'
 app.debug = True
 
+# # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+# database connection
 urlparse.uses_netloc.append("postgres")
 url = urlparse.urlparse(os.environ["DATABASE_URL"])
 conn = psycopg2.connect(
@@ -32,21 +39,35 @@ conn = psycopg2.connect(
    port=url.port
 )
 
+# # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+
+def fetch_data(conn, query, params):
+    cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+    
+    cur.execute(query, params)
+    data = cur.fetchall()
+    
+    return data
+
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # #
 
 # Requires authentication annotation
 def requires_auth(f):
-  @wraps(f)
-  def decorated(*args, **kwargs):
+    @wraps(f)
+    def decorated(*args, **kwargs):
     if 'profile' not in session:
-      return redirect('/')
+        return redirect('/')
     return f(*args, **kwargs)
 
-  return decorated
+    return decorated
 
 
 @app.route('/public/<path:filename>')
 def static_files(filename):
     return send_from_directory('./public', filename)
+
 
 # Controllers API
 @app.route("/")
@@ -68,8 +89,12 @@ def dashboard():
         'tc' : 92,
         'art': '34 sec'
     }
-
-    return render_template('dashboard.html', user=session['profile'], user_data=user_data)
+    query = '''SELECT * FROM dashboard_users WHERE user_id = %s '''
+    
+    if fetch_data(conn, query, (session['profile']['user_id'], )):
+        return render_template('dashboard.html', user=session['profile'], user_data=user_data)
+    else:
+        return "Forbidden"
 
 
 @app.route('/logout')
@@ -81,29 +106,29 @@ def logout():
 
 @app.route('/callback')
 def callback_handling():
-  code = request.args.get('code')
+    code = request.args.get('code')
 
-  json_header = {'content-type': 'application/json'}
+    json_header = {'content-type': 'application/json'}
 
-  token_url = "https://{domain}/oauth/token".format(domain=env["AUTH0_DOMAIN"])
-  token_payload = {
-    'client_id'     : env['AUTH0_CLIENT_ID'],
-    'client_secret' : env['AUTH0_CLIENT_SECRET'],
-    'redirect_uri'  : env['AUTH0_CALLBACK_URL'],
-    'code'          : code,
-    'grant_type'    : 'authorization_code'
-  }
+    token_url = "https://{domain}/oauth/token".format(domain=env["AUTH0_DOMAIN"])
+    token_payload = {
+        'client_id'     : env['AUTH0_CLIENT_ID'],
+        'client_secret' : env['AUTH0_CLIENT_SECRET'],
+        'redirect_uri'  : env['AUTH0_CALLBACK_URL'],
+        'code'          : code,
+        'grant_type'    : 'authorization_code'
+        }
 
-  token_info = requests.post(token_url, data=json.dumps(token_payload), headers = json_header).json()
+    token_info = requests.post(token_url, data=json.dumps(token_payload), headers = json_header).json()
 
-  user_url = "https://{domain}/userinfo?access_token={access_token}"  \
+    user_url = "https://{domain}/userinfo?access_token={access_token}"  \
     .format(domain=env["AUTH0_DOMAIN"], access_token=token_info['access_token'])
 
-  user_info = requests.get(user_url).json()
+    user_info = requests.get(user_url).json()
 
-  session['profile'] = user_info
+    session['profile'] = user_info
 
-  return redirect('/dashboard')
+    return redirect('/dashboard')
 
 
 
